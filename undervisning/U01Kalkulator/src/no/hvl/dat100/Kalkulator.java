e9e7c63c4cce914da8f52f3770408364b4cddac9
e184940 <184940@stud.hvl.no>
2024-08-19 12:58:51 +0200

    Update Kalkulator.java

--- undervisning/U01Kalkulator/src/no/hvl/dat100/Kalkulator.java
+++ undervisning/U01Kalkulator/src/no/hvl/dat100/Kalkulator.java
@@ -3,26 +3,22 @@ package no.hvl.dat100;
 public class Kalkulator {
 
 	public static int add(int a, int b) {
-		
-		// TODO
-		throw new RuntimeException("add ikke implementert");
+	
+		return a+b;
 	}
 
 	public static int sub(int a, int b) {
 		
-		// TODO
-		throw new RuntimeException("sub ikke implementert");
+		return a-b;
 	}
 
 	public static int mul(int a, int b) {
 		
-		// TODO
-		throw new RuntimeException("mul ikke implementert");
+		return a*b;
 	}
 
 	public static int div(int a, int b) {
 		
-		// TODO
-		throw new RuntimeException("div ikke implementert");
+		return a/b;
 	}
 }
